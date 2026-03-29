baa20bac4b6299d2f08fe4d5cfab2b627f80660d
Shubh Tiwari <shubhtiwari9569@gmail.com>
2026-03-29 16:21:17 +0530

    update variable

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -3,7 +3,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const link = new HttpLink({
-  uri: process.env.BACKENDURL,
+  uri: process.env.NEXT_PUBLIC_BACKEND_URL,
 });
 
 export const client = new ApolloClient({
